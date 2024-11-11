3cafb578a974fb7342830297fff126cdf5d76eb6
Jarcan <171005028@qq.com>
2024-11-11 13:51:56 +0800

    feat: 初始化预算管理项目
    - 添加 Spring Boot 应用配置文件
    - 创建主应用类和测试类
    - 配置数据库、Redis 和 MyBatis 等依赖
    - 设置日志级别

---
+++ src/main/java/org/psd/budget_management/BudgetManagementApplication.java
@@ -0,0 +1,16 @@
+package org.psd.budget_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pengshidun
+ */
+@SpringBootApplication
+public class BudgetManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetManagementApplication.class, args);
+    }
+
+}
