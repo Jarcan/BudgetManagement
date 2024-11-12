e84cafb8b60280f10eb093a7ec92e3e211db5ae9
Jarcan <171005028@qq.com>
2024-11-12 19:34:43 +0800

    feat(budget-management): 增加预算费用变更和调整功能- 在 BudgetCostController 中添加了 changeBudgetCost 和 adjust 方法 - 在 BudgetCostService 接口中定义了新的变更和调整预算费用的方法 - 在 BudgetCostServiceImpl 中实现了预算费用变更和调整的逻辑- 修改了 BudgetCost 实体类中的字段名，将 availableBalanceAsh 改为 availableBalanceCash - 更新了 BudgetCostMapper.xml 中的映射关系

--- src/main/java/org/psd/budget_management/service/BudgetCostService.java
+++ src/main/java/org/psd/budget_management/service/BudgetCostService.java
@@ -2,7 +2,9 @@ package org.psd.budget_management.service;
 
 import org.psd.budget_management.entity.BudgetCost;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.psd.budget_management.entity.Result;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -20,4 +22,28 @@ public interface BudgetCostService extends IService<BudgetCost> {
      * @return 修改结果
      */
     Boolean updateStatus(List<Integer> idList, Integer status);
+
+    /**
+     * 变更预算费用
+     *
+     * @param budgetCostId   预算费用id
+     * @param budgetCostType 预算类型
+     * @param changeType     变更类型
+     * @param amount         金额
+     * @param description    描述
+     * @return status 变更结果
+     */
+    Result changeBudgetCost(Integer budgetCostId, String budgetCostType, String changeType, BigDecimal amount, String description);
+
+    /**
+     * 调整预算费用金额到另一个预算费用
+     *
+     * @param budgetCostId       预算费用ID，用于标识特定的预算费用记录
+     * @param targetBudgetCostId 目标预算费用ID，用于关联调整操作的预算费用
+     * @param budgetCostType     预算费用类型，描述费用的类别
+     * @param amount             调整金额，要增加或减少的具体金额
+     * @param description        描述，对此次调整的详细说明
+     * @return 调整操作的结果，包括是否成功以及相关的消息
+     */
+    Result adjust(Integer budgetCostId, Integer targetBudgetCostId, String budgetCostType, BigDecimal amount, String description);
 }
